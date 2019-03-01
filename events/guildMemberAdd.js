98ccd755381f2e868481d92aafe735014eeaedbf
Alfredo Fialho <alfoltran@gmail.com>
2019-03-01 10:13:56 -0300

    Corrigindo mensagem de boas-vindas.

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -3,7 +3,7 @@ module.exports = (client, member) => {
 
     if (settings.welcomeEnabled !== "true") return;
 
-    const welcomeMessage = settings.welcomeMessage.replace("{{user}}", member.user.tag);
+    const welcomeMessage = settings.welcomeMessage.replace("{{user}}", member.user.username);
 
     member.guild.channels.find(c => c.name === settings.welcomeChannel).send(welcomeMessage).catch(console.error);
 };
