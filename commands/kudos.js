30b613b37b493e3a12bd1e6228cecadc7d7457e2
Alfredo Fialho <alfoltran@gmail.com>
2019-06-12 11:45:08 -0300

    Evoluindo a implementação de "feedbacks".

--- commands/kudos.js
+++ commands/kudos.js
@@ -1,19 +1,21 @@
 // noinspection JSUnusedLocalSymbols
 exports.run = async (client, message, args, level) => {
-    if (!args || args.length === 1) {
+    if (!args || args.length >= 2) {
+        let user = message.mentions.members.first();
+        let kudos = args.slice(1).join(" ");
         message.channel.send({
             embed: {
                 color: 3447003,
                 author: {
-                    name: client.user.username,
-                    icon_url: client.user.avatarURL
+                    name: message.user.username,
+                    icon_url: message.user.avatarURL
                 },
                 title: "Kudos",
                 url: "https://www.google.com/search?q=kudos+feedback",
                 description: "Real-time Continuous Peer Feedback",
                 fields: [{
-                    name: "Good Job! " + args[0],
-                    value: "Em breve teremos mais uma funcionalidade aqui."
+                    name: "Good Job " + user.nickname,
+                    value: kudos
                 }],
                 thumbnail: {
                     url: "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/198/military-medal_1f396.png",
