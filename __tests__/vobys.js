65b4bdc7004fa19c450b7a52fd83f5e3b66a51fa
Alfredo Fialho <alfoltran@gmail.com>
2020-03-16 18:24:46 -0300

    :rotating_light: Configurando ESLint no projeto.

---
+++ __tests__/vobys.js
@@ -0,0 +1,11 @@
+"use strict";
+
+describe("vobys-bot:app", () => {
+  beforeAll(() => {
+    console.log("Testing Vobys Bot...");
+  });
+
+  it("NYI", () => {
+    expect(true).toEqual(true);
+  });
+});
